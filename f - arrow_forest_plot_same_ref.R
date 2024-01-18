dcd90e55cbcf9415e3fa59267d783cb2746f2470
Vy Nguyen <nguyenvy@umich.edu>
2024-01-18 14:59:26 -0500

    create arrow forest plot with same reference group

---
+++ f - arrow_forest_plot_same_ref.R
@@ -0,0 +1,8 @@
+arrow_forest_plot_same_ref <- function(list_all
+                                        , list_stratified
+                                        , name_of_folder 
+                                        , current_directory
+                                        , is_adult = TRUE)
+{
+  
+}
