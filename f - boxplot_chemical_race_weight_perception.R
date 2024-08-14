680e636c229df007dea3e297ba5c06d30d1a7f00
Vy Nguyen <nguyenvy@umich.edu>
2024-08-14 14:41:13 -0400

    normalized by creatinine levels in boxplots

--- f - boxplot_chemical_race_weight_perception.R
+++ f - boxplot_chemical_race_weight_perception.R
@@ -35,12 +35,11 @@ boxplot_chemical_race_weight_perception <- function(df_nhanes
            , "WT_URXBP3"
            , covariates) %>%
     na.omit(.) %>%
-    # filter(race != "All NHANES Women") %>%
     mutate(race = factor(race
                          , levels = c("All NHANES Women"
                                       , "Non-Hispanic White"
                                       , "Non-Hispanic Black"
-                                      , "Mexican American"                   
+                                      , "Mexican American"
                                       , "Other Hispanic"
                                       , "Non-Hispanic Asian"
                                       , "Other Race - Including Multi-Racial"))) %>%
@@ -50,18 +49,34 @@ boxplot_chemical_race_weight_perception <- function(df_nhanes
     mutate(race_weight_perception = factor(race_weight_perception)) %>%
     mutate(weight_perception = factor(weight_perception
                                       , levels = c("_overweight"
-                                                   , "_about the right weight")))
+                                                   , "_about the right weight"))) %>%
+    mutate(converted_creatinine = URXUCR/100) # 100 mL in 1 dL
   
-  # print(dim(df_nhanes_same_size))
+  chem_conc_normalized_creatinine <- df_nhanes_same_size[,chemical]/df_nhanes_same_size[,"converted_creatinine"]
+
+  df_nhanes_same_size <- df_nhanes_same_size %>%
+    mutate(chem_conc_normalized_creatinine = chem_conc_normalized_creatinine)
   
+  # print(colnames(df_nhanes_same_size))
   
-  chemical_name <- df_dictionary %>%
-    filter(variable_codename_use == chemical) %>%
-    pull(variable_description_use)
+  # View(df_nhanes_same_size %>%
+  #        select(URXBP3
+  #               , URXUCR
+  #               , converted_creatinine
+  #               , chem_conc_normalized_creatinine) %>%
+  #        unique(.))
   
+  attr(df_nhanes_same_size$chem_conc_normalized_creatinine,"label") <- "Benzophenone-3 (ng/mg creatinine)"
+  
+  # chemical_name <- df_dictionary %>%
+  #   filter(variable_codename_use == chemical) %>%
+  #   pull(variable_description_use)
+  
+  chemical_name <- attr(df_nhanes_same_size$chem_conc_normalized_creatinine,"label")
+
   boxplot_race_same_size <- ggplot(data = df_nhanes_same_size
                                    , mapping = aes(x = race
-                                                   , y = df_nhanes_same_size[,chemical]
+                                                   , y = chem_conc_normalized_creatinine
                                                    , fill = race)) +
     geom_boxplot() +
     # facet_grid(cols = vars(race)) +
@@ -75,13 +90,13 @@ boxplot_chemical_race_weight_perception <- function(df_nhanes
     theme(legend.position = "top"
           , axis.text.x = element_blank()
           , axis.title.x = element_blank())
-  
+
   df_stats_sample_size <- df_nhanes_same_size %>%
     group_by(race, weight_perception) %>%
     summarise(n = n()) %>%
     ungroup(.)
   View(df_stats_sample_size)
-  
+
   df_sample_size_label <- df_stats_sample_size %>%
     mutate(weight_perception = gsub(" "
                                     , "_"
@@ -95,21 +110,21 @@ boxplot_chemical_race_weight_perception <- function(df_nhanes
                          , about_the_right_weight
                          , sep = "       "))
   # print(df_sample_size_label)
-  
+
   df_nhanes_same_size <- full_join(df_nhanes_same_size
                                   , df_sample_size_label %>%
                                     select(race
                                            , label)
                                   , by = "race")
-  
+
   boxplot_race_weight_perception_same_size <- ggplot(data = df_nhanes_same_size
                                                      , mapping = aes(x = weight_perception
-                                                                     , y = df_nhanes_same_size[,chemical]
+                                                                     , y = chem_conc_normalized_creatinine
                                                                      , fill = weight_perception)) +
     geom_violin() +
     geom_boxplot(data = df_nhanes_same_size
                  , mapping = aes(x = weight_perception
-                                 , y = df_nhanes_same_size[,chemical]
+                                 , y = chem_conc_normalized_creatinine
                                  )
                  , inherit.aes = FALSE
                  , width = 0.3) +
@@ -122,7 +137,7 @@ boxplot_chemical_race_weight_perception <- function(df_nhanes
     facet_grid(cols = vars(race)) +
     geom_text(data = df_nhanes_same_size
               , mapping = aes(x = 1.5
-                              , y = max(df_nhanes_same_size[,chemical]) + 10000
+                              , y = max(df_nhanes_same_size[,"chem_conc_normalized_creatinine"]) + 10000
                               , label = label)) +
     geom_signif(comparisons = list(c("_overweight"
                                      , "_about the right weight"))
@@ -145,13 +160,13 @@ boxplot_chemical_race_weight_perception <- function(df_nhanes
           , axis.title.y = element_text(size = 12)
           , legend.title = element_text(size = 12)
           , legend.text = element_text(size = 12))
-  
+
   if(is_adult == TRUE) {
     plot_name.png <- paste("box_plot_"
                          , "same_sample_size"
                          , ".png"
                          , sep = "")
-  
+
     plot_name.pdf <- paste("box_plot_"
                          , "same_sample_size"
                          , ".pdf"
@@ -161,7 +176,7 @@ boxplot_chemical_race_weight_perception <- function(df_nhanes
                            , "same_sample_size"
                            , ".png"
                            , sep = "")
-    
+
     plot_name.pdf <- paste("box_plot_youth_"
                            , "same_sample_size"
                            , ".pdf"
@@ -180,55 +195,24 @@ boxplot_chemical_race_weight_perception <- function(df_nhanes
          , width = 15
          , height = 9
          , units = "in")
-  
-  df_nhanes_ref_black <- create_dataset_with_reference_as_black(df_nhanes_same_size)
-  
-  # View(df_nhanes_ref_black %>% select(race, weight_perception) %>% unique())
-  
-  boxplot_race_weight_perception_same_size_ref_black <- ggplot(data = df_nhanes_ref_black
-                                                               , mapping = aes(x = weight_perception
-                                                                               , y = df_nhanes_ref_black[,chemical]
-                                                                               , fill = weight_perception)) +
-    geom_boxplot() +
-    geom_jitter() +
-    facet_grid(cols = vars(race)) +
-    stat_summary(fun.y = mean
-                 , geom = "point"
-                 , shape = 24
-                 , size = 4
-                 , fill = "brown") +
-    # geom_signif(comparisons = list(c("_overweight"
-    #                                  , "_about the right weight")), 
-    #             map_signif_level = TRUE) +
-    scale_y_log10() +
-    # scale_fill_manual(values = c(#"black"
-    #   "red"
-    #   , "blue")
-    #   , labels = c(#"All"
-    #     "Perceived overweight"
-    #     , "Perceived at the right weight"
-    #   )) +
-    labs(y = chemical_name) +
-    theme(legend.position = "top"
-          , axis.text.x = element_blank()
-          , axis.title.x = element_blank())
-  
+
   df_nhanes_max_size <- df_nhanes %>%
     select(weight_perception
            , chemical
-           , race) %>%
+           , race
+           , URXUCR) %>%
     na.omit(.) %>%
     mutate(weight_perception = factor(weight_perception
                                       , levels = c("_overweight"
                                                    , "_about the right weight")))
-  print(dim(df_nhanes_max_size))
-  
+  # print(dim(df_nhanes_max_size))
+
   df_stats_sample_size <- df_nhanes_max_size %>%
     group_by(race, weight_perception) %>%
     summarise(n = n()) %>%
     ungroup(.)
   # View(df_stats_sample_size)
-  
+
   df_sample_size_label <- df_stats_sample_size %>%
     mutate(weight_perception = gsub(" "
                                     , "_"
@@ -242,19 +226,31 @@ boxplot_chemical_race_weight_perception <- function(df_nhanes
                          , about_the_right_weight
                          , sep = "    "))
   # print(df_sample_size_label)
-  
+
   df_nhanes_max_size <- full_join(df_nhanes_max_size
                                   , df_sample_size_label %>%
                                     select(race
                                            , label)
-                                  , by = "race")
-  
-  boxplot_max_size <- ggplot(data = df_nhanes_max_size 
+                                  , by = "race") %>%
+    mutate(converted_creatinine = URXUCR/100) # 100 mL in 1 dL
+
+  chem_conc_normalized_creatinine <- df_nhanes_max_size[,chemical]/df_nhanes_max_size[,"converted_creatinine"]
+
+  df_nhanes_max_size <- df_nhanes_max_size %>%
+    mutate(chem_conc_normalized_creatinine = chem_conc_normalized_creatinine)
+
+  boxplot_max_size <- ggplot(data = df_nhanes_max_size
                              , mapping = aes(x = weight_perception
-                                             , y = df_nhanes_max_size[,chemical]
+                                             , y = chem_conc_normalized_creatinine
                                              , fill = weight_perception)) +
-    geom_boxplot() +
-    geom_jitter() +
+    geom_violin() +
+    geom_boxplot(data = df_nhanes_same_size
+                 , mapping = aes(x = weight_perception
+                                 , y = chem_conc_normalized_creatinine
+                 )
+                 , inherit.aes = FALSE
+                 , width = 0.3) +
+    # geom_jitter() +
     facet_grid(cols = vars(race)) +
     geom_signif(comparisons = list(c("_overweight"
                                      , "_about the right weight")),
@@ -266,7 +262,7 @@ boxplot_chemical_race_weight_perception <- function(df_nhanes
                  , fill = "brown") +
     geom_text(data = df_nhanes_max_size
               , mapping = aes(x = 1.5
-                              , y = max(df_nhanes_max_size[,chemical]) + 10000
+                              , y = max(chem_conc_normalized_creatinine) + 10000
                               , label = label)) +
     scale_y_log10() +
     scale_fill_manual(values = c("red"
@@ -277,13 +273,13 @@ boxplot_chemical_race_weight_perception <- function(df_nhanes
     theme(legend.position = "top"
           , axis.text.x = element_blank()
           , axis.title.x = element_blank())
-  
+
   if(is_adult==TRUE) {
     plot_name.png <- paste("box_plot_"
                          , "max_sample_size"
                          , ".png"
                          , sep = "")
-  
+
     plot_name.pdf <- paste("box_plot_"
                          , "max_sample_size"
                          , ".pdf"
@@ -293,12 +289,12 @@ boxplot_chemical_race_weight_perception <- function(df_nhanes
                            , "max_sample_size"
                            , ".png"
                            , sep = "")
-    
+
     plot_name.pdf <- paste("box_plot_youth_"
                            , "max_sample_size"
                            , ".pdf"
                            , sep = "")
-    
+
   }
   # Save the panel of stairway plots as a png and pdf
   print(plot_name.png)
@@ -313,8 +309,10 @@ boxplot_chemical_race_weight_perception <- function(df_nhanes
          , width = 14
          , height = 9
          , units = "in")
-  
+
   # Set the directory to the folder containing the function and main scripts
   setwd(current_directory)
-  boxplot_race_weight_perception_same_size
+  # boxplot_race_weight_perception_same_size
+  # boxplot_race_same_size
+  boxplot_max_size
 }
